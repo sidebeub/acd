6d985531891fa872c3acbefd068829f40128c581
sidebeub <zack.4290@gmail.com>
2026-01-22 13:53:54 -0800

    Downgrade to Prisma 6 for Node compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
-
-const connectionString = process.env.DATABASE_URL
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
